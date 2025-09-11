242e1271311664879420b89d2db634eb3ec2f45b
Varis Vitols <varisvitols@gmail.com>
2025-09-11 12:32:57 +0300

    Create the Product API

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
+import { useEffect } from 'react';
+import { fetchProducts } from './api/productApi';
+
 function App() {
+  useEffect(() => {
+    fetchProducts().then((products) => console.log('products', products));
+  }, []);
+
   return (
     <>
       <p>Hello, World!</p>
